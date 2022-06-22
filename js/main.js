cf182f8effec3f4007a75b251c05cd0fddd914e2
John Soares <johnsued25@gmail.com>
2022-06-22 10:52:52 -0300

    final project

--- js/main.js
+++ js/main.js
@@ -1,47 +1,92 @@
+/* Operador lógico que retorna com dados salvos, ou string vazia, 
+utilizando localStorage.getItem, modificando o 
+valor de `string` com JSON.parse()*/
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+/*// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página */
 itens.forEach(elemento => {
-    console.log(elemento.nome, elemento.quantidade)
-});
+    criaElemento(elemento)
+})
 
+/*// Refatoração do addEventListener para receber as funções extras da função criaElemento*/
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-    //Crie variáveis para acessar os valores enviados:
+    
     const nome = evento.target.elements["nome"]
     const quantidade = evento.target.elements["quantidade"]
+
+    //  Const para conferir elemento nome no array itens
+    const existe = itens.find(elemento => elemento.nome === nome.value)
     
-    criaElemento(nome.value, quantidade.value);//função chamando os eventos "nome ,quantidade"
+    const itemAtual = {
+        "nome": nome.value, 
+        "quantidade": quantidade.value
+    }
+
+    // Condicional para conferir se o elemento 
+    if (existe) {
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    }else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0;
+
+        criaElemento(itemAtual)
     
-    //Refatoração do código para que após o envio de itens, o formulário fique vazio:
+        itens.push(itemAtual)
+    }
+
+  
+    
+  
+    
+    localStorage.setItem("itens", JSON.stringify(itens))
+    
     nome.value = ""
     quantidade.value = ""
 });
-//função cria elemento
-function criaElemento(nome, quantidade) {
+
+/*// Refatoração da função `criaElemento` para que possua apenas a função que faça sentido ao nome. */
+function criaElemento(item) {
     // <li class="item"><strong>7</strong>Camisa</li>
     const novoItem = document.createElement("li");
     novoItem.classList.add("item");
 
     //criando novo item para Numero strong....
     const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade
-
-    //appendChild manipulando os dados para mostrar a tela..
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)//mandando item camisa branca para html
-    novoItem.innerHTML += nome //recebendo item da camisa branca no html
+
+    
+    novoItem.innerHTML += item.nome //recebendo item da camisa branca no html
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem);
+}
 
-    /*Transforme a variável já criada, itemAtual, em um objeto object que receba os valores de nome e quantidade,
-     e transforme os valores estes valores em string:*/
-    const itemAtual = {
-        "nome": nome, 
-        "quantidade": quantidade
-    }
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
 
-    itens.push(itemAtual)//Insira a variável itemAtual nesse array itens, utilizando o método push:
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "X"
 
-    localStorage.setItem("itens", JSON.stringify(itens));//transformando objeto para uma string no localStorage
+    elementoBotao.addEventListener("click", function () {
+        deletaElelemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElelemento(tag, id) {
+    tag.remove()
+    console.log(id)
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
